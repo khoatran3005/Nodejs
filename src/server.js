0d8258423d469595db279c65ef21784bd33f3c5f
Khoa Tran <khoatran3005@gmail.com>
2023-05-17 20:15:46 -0400

    seventh

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
-const mysql = require('mysql2');
+const connection = require('./config/database')
 
 const app = express()
 const port = process.env.PORT || 8888;
@@ -13,20 +13,10 @@ configViewEngine(app);
 
 app.use('/', webRoutes);
 
-//test connection
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'hoidanit',
-    port: 3307,
-    password: '123456'
-});
-
 connection.query(
     'SELECT * FROM Users u',
     function (err, results, fields) {
         console.log(results); // results contains rows returned by server
-        console.log(fields); // fields contains extra meta data about results, if available
     }
 );
 
