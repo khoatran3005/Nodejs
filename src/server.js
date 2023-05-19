0da33f00995a160c24c89e0cc1127caf597bd314
Khoa Tran <khoatran3005@gmail.com>
2023-05-19 18:01:20 -0400

    33rd

--- src/server.js
+++ src/server.js
@@ -4,10 +4,15 @@ const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
 const routerAPI = require('./routes/api')
 const connection = require('./config/database')
+const fileUpload = require('express-fileupload');
+
 
 const app = express()
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
+
+//config file upload
+app.use(fileUpload());
 
 //config req.body
 app.use(express.json()); // Used to parse JSON bodies
