fb4e6603ef0390b67dcae0936e3bf91126de6c9d
Khoa Tran <khoatran3005@gmail.com>
2023-05-18 22:53:43 -0400

    24th

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,32 @@
-const mysql = require('mysql2/promise');
+const mongoose = require('mongoose');
 require('dotenv').config()
 
-const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    password: process.env.DB_PASSWORD,
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10,
-    queueLimit: 0
-});
+var dbState = [{
+    value: 0,
+    label: "disconnected"
+},
+{
+    value: 1,
+    label: "connected"
+},
+{
+    value: 2,
+    label: "connecting"
+},
+{
+    value: 3,
+    label: "disconnecting"
+}];
+
+
+const connection = async () => {
+    try {
+        await mongoose.connect('mongodb://root:123456@localhost:27018');
+        const state = Number(mongoose.connection.readyState);
+        console.log(dbState.find(f => f.value == state).label, "to db");
+    } catch (error) {
+        console.log("error", error);
+    }
+}
 
 module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
 const connection = require('./config/database')
 
-
 const app = express()
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
@@ -17,6 +16,8 @@ app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
 configViewEngine(app);
 
 app.use('/', webRoutes);
+
+connection();
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
